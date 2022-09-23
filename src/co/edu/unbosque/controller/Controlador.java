ef8fabe9e6e193e3e900009b25b71274c23c8204
Karen Buitrago <karenbuitrago2519@gmail.com>
2022-09-22 22:06:49 -0500

    Por fin un controlador

---
+++ src/co/edu/unbosque/controller/Controlador.java
@@ -0,0 +1,7 @@
+package co.edu.unbosque.controller;
+
+public class Controlador {
+	public void iniciar() {
+		System.out.println("Estoy en el controlador");
+	}
+}
--- src/co/edu/unbosque/controller/MainApp.java
+++ src/co/edu/unbosque/controller/MainApp.java
@@ -1,9 +1,12 @@
 package co.edu.unbosque.controller;
 
 public class MainApp {
+	
 
 	public static void main(String[] args) {
 		System.out.println("Clase de git");
+		Controlador controlador = new Controlador();
+		controlador.iniciar();
 	}
 
 }
